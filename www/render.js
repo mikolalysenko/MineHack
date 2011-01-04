7736879075d6677f818f0d45ca90fe5a47d24e2b
Mikola <mikola@STUPIDHOSTNAME.(none)>
2011-01-04 16:44:24 -0600

    fixed rendering bug

--- www/render.js
+++ www/render.js
@@ -7,14 +7,22 @@ Render.init = function(canvas)
 {
 	Render.canvas = canvas;
 
+	var gl;
 	try
 	{
-		Render.gl = canvas.getContext("experimental-webgl");
+		gl = canvas.getContext("experimental-webgl");
 	}
 	catch(e)
 	{
 		return 'Browser does not support WebGL';
 	}
+	
+	if(!gl)
+	{
+		return 'Invalid WebGL object';
+	}
+	
+	Render.gl = gl;
 
 	width = canvas.width;
 	height = canvas.height;
@@ -22,7 +30,7 @@ Render.init = function(canvas)
 	gl = Render.gl;
 	gl.viewport(0, 0, width, height);
 	
-	gl.clearColor(0, 0, 0, 0);
+	gl.clearColor(0.0, 0.0, 0.0, 1.0);
 	gl.clear(gl.COLOR_BUFFER_BIT |gl.DEPTH_BUFFER_BIT);
 	gl.flush();
 	
