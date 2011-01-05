3dff58de0eb5958135f8dbd00dcbe1993b4e8327
mikolalysenko <mikolalysenko@gmail.com>
2011-01-04 21:15:41 -0600

    adding changes

---
+++ www/game.js
@@ -0,0 +1,33 @@
+Game = {}
+
+Game.init = function(canvas)
+{
+	Game.canvas = canvas;
+
+	var gl;
+	try
+	{
+		gl = canvas.getContext("experimental-webgl");
+	}
+	catch(e)
+	{
+		return 'Browser does not support WebGL,';
+	}
+	
+	if(!gl)
+	{
+		return 'Invalid WebGL object';
+	}
+	
+	Game.gl = gl;
+
+	width = canvas.width;
+	height = canvas.height;
+	
+	gl.viewport(0, 0, width, height);
+	gl.clearColor(0.0, 0.0, 0.0, 1.0);
+	gl.clear(gl.COLOR_BUFFER_BIT |gl.DEPTH_BUFFER_BIT);
+	gl.flush();
+	
+	return 'Ok';
+}
--- www/render.js
+++ www/render.js
@@ -5,35 +5,5 @@ var Render =
 //Initialize the OpenGL contet
 Render.init = function(canvas)
 {
-	Render.canvas = canvas;
-
-	var gl;
-	try
-	{
-		gl = canvas.getContext("experimental-webgl");
-	}
-	catch(e)
-	{
-		return 'Browser does not support WebGL';
-	}
 	
-	if(!gl)
-	{
-		return 'Invalid WebGL object';
-	}
-	
-	Render.gl = gl;
-
-	width = canvas.width;
-	height = canvas.height;
-	
-	gl = Render.gl;
-	gl.viewport(0, 0, width, height);
-	
-	gl.clearColor(0.0, 0.0, 0.0, 1.0);
-	gl.clear(gl.COLOR_BUFFER_BIT |gl.DEPTH_BUFFER_BIT);
-	gl.flush();
-	
-	
-	return 'Ok';
 };
