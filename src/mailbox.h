1d981b2fe0174b4b9dfffc18349fc5051a5fef4d
mikola <mikola@sal-0019.(none)>
2011-01-20 10:03:14 -0600

    going to overhaul the update handler on the server (again).  right now it does too many wasteful memcpy's, and so it is not terribly efficient.  the new version will kill off the UpdateEvent object and instead handle events by directly posting them to the mailbox.  this should allow some additional compression for entity coordinates, since they can then be encoded as relative transformations to some pre-specified origin.

--- src/mailbox.h
+++ src/mailbox.h
@@ -14,10 +14,29 @@
 #include "chunk.h"
 #include "input_event.h"
 #include "entity.h"
+#include "update_event.h"
 
 namespace Game
 {
-
+	struct UpdateMailbox
+	{
+		UpdateMailbox();
+		~UpdateMailbox();
+	
+		//Sends an event to a terget
+		void send_event(EntityID const& target, UpdateEvent const&);
+		
+		//Retrieves all events
+		void* get_events(EntityID const& target, int& len);
+		
+		//Clears all pending events for target entity
+		void clear_events(EntityID const& target);
+		
+	private:
+		
+		pthread_spinlock_t	lock;
+		TCMAP*				mailboxes;
+	};
 };
 
 
--- src/update_event.h
+++ src/update_event.h
@@ -87,25 +87,7 @@ namespace Game
 		int write(uint8_t*) const;
 	};
 	
-	struct UpdateMailbox
-	{
-		UpdateMailbox();
-		~UpdateMailbox();
-	
-		//Sends an event to a terget
-		void send_event(EntityID const& target, UpdateEvent const&);
-		
-		//Retrieves all events
-		void* get_events(EntityID const& target, int& len);
-		
-		//Clears all pending events for target entity
-		void clear_events(EntityID const& target);
-		
-	private:
-		
-		pthread_spinlock_t	lock;
-		TCMAP*				mailboxes;
-	};
+
 };
 
 #endif
